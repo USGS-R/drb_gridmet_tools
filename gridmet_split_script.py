68a5718d597a9424835d915775d7962a309eb83f
Margaux Sleckman <msleckman@usgs.gov>
2022-01-25 14:52:50 -0800

    finalizing split script

--- gridmet_split_script.py
+++ gridmet_split_script.py
@@ -2,40 +2,71 @@
 import pickle
 import time
 import geopandas as gpd
+import geopandas.geodataframe
 import grd2shp_xagg
 import xagg as xa
 import xarray as xr
 import os
+import datetime
 
 # Variables
-gdf_nhru02_path = './data/nhru_02/nhru_02.shp'
-gdf = gpd.read_file(gdf_nhru02_path)
-start_date = '1979-01-01'
-end_date = '1980-01-01'
-lon_min, lat_min, lon_max, lat_max = gdf.total_bounds
-output_path = './data/'
 
 ## official list of variables needed for drb-inland-salinity model
 data_vars_shrt_all = ['tmmx', 'tmmn', 'pr', 'srad', 'vs','rmax','rmin','sph']
+## drb catchment polygons
+gdf_nhru02_path = './data/nhru_02/nhru_02.shp'
+gdf = gpd.read_file(gdf_nhru02_path)
+## date range
+start_date = '1979-01-01'
+end_date = '2021-01-01'
+## lat lon
+lon_min, lat_min, lon_max, lat_max = gdf.total_bounds
+## output folder
+output_path = './data/'
 
-def get_gridmet_datasets(variables, start_date, end_date, polygon_bbox = None, lon_min = None, lat_min = None, lon_max = None, lat_max = None):
 
-    ## check bounds for data slicing
-    if polygon_bbox is not None:
-        lon_min, lat_min, lon_max,lat_max = polygon_bbox
-        print(lon_min, lat_min, lon_max,lat_max)
+def get_gridmet_datasets(variable, start_date, end_date, polygon_as_bbox = None, lon_min = None, lat_min = None, lon_max = None, lat_max = None):
+
+    """
+    :param str/list var_short: data variable short name or list of data variable short names. Must be one of the following: ['tmmx', 'tmmn', 'pr', 'srad', 'vs', 'rmax', 'rmin', 'sph']
+    :param str start_date: Start date of data collection yyyy-mm-dd
+    :param str end_date: End date of data collection yyyy-mm-dd
+    :param gdf.total_bounds polygon_bbox: total bounds of a geodataframe. Ex: geodataframe.total_bounds. If not known, None
+    :param str lon_min: bbox of aoi longitude min. None if polygon_bbox is given (i.e. polygon_bbox != None)
+    :param str lat_min: bbox of aoi latitude min. None if polygon_bbox is given (i.e. polygon_bbox != None)
+    :param str lon_max: bbox of aoi longitude max. None if polygon_bbox is given (i.e. polygon_bbox != None)
+    :param str lat_max: bbox of aoi latitude max. None if polygon_bbox is given (i.e. polygon_bbox != None)
+    :return: dictionary of xarray stored by variable name
+    """
+
+    ## check/define bounds for data slicing
+    if polygon_as_bbox is not None:
+        if isinstance(polygon_as_bbox, geopandas.geodataframe.GeoDataFrame):
+            print('polygon is geodataframe')
+            pass
+        elif os.path.isfile(polygon_as_bbox) == True:
+            print('polygon is path to shapefile')
+            polygon_as_bbox = gpd.read_file(polygon_as_bbox)
+        else:
+             raise TypeError('polygon_as_bbox should str path to shapefile or geodataframe')
+
+        if polygon_as_bbox.crs != 'EPSG:4326':
+            polygon_as_bbox = polygon_as_bbox.to_crs('EPSG:4326')
+            print('geodataframe crs changed to EPSG:4326')
+
+        lon_min, lat_min, lon_max, lat_max = polygon_as_bbox.total_bounds
     else:
-        print(lon_min, lat_min, lon_max,lat_max)
+        pass
 
-    ## check if only 1 var (short) is provided, change to list
-    if not isinstance(variables, list):
-        variables = [variables]
-        print(variables)
+    ## if only 1 var (short) is provided, change to list
+    if not isinstance(variable, list):
+        variable = [variable]
 
-    ## empty list for the xarray.datasets
-    data_xr_ds_list = []
+    ## Initiate list of empty datasets
+    xarray_dict = dict()
 
-    for var in variables:
+    ## Loop through variables and pull data + place in dict
+    for var in variable:
         ## Pulling data
         # source: http://thredds.northwestknowledge.net:8080/thredds/reacch_climate_MET_aggregated_catalog.html
         url = f'http://thredds.northwestknowledge.net:8080/thredds/dodsC/agg_met_{var}_1979_CurrentYear_CONUS.nc'
@@ -50,67 +81,81 @@ def get_gridmet_datasets(variables, start_date, end_date, polygon_bbox = None, l
         end = time.perf_counter()
         print(f'finish in {round(start - end, 2)} seconds')
 
-        # Append to list of xr.datasets
-        data_xr_ds_list.append(ds_subset)
-
-    ## Creating output dictionary
-    xarray_dict = dict(zip(variables, data_xr_ds_list))
+        # Append to dict of xr.datasets
+        xarray_dict[var] = ds_subset
 
     return xarray_dict
 
-xarray_dict = get_gridmet_datasets(variables = data_vars_shrt_all,
-                                   start_date = start_date, end_date = end_date,
-                                   polygon_bbox = gdf.total_bounds)
-#                     lon_min = -80, lat_min = 36, lon_max = -71, lat_max = 45
 
 def create_weightmap(polygon, xarray_dict, output_data_folder, weightmap_var = None):
 
+    """
+    :param geodataframe polygon: geodataframe polygon or multipolygon
+    :param dict xarray_dict: dictionary of gridmet data. the output of get_gridmet_datasets()
+    :param str output_data_folder: path to folder for function output
+    :param str weightmap_var: variable used to make weightfile + naming of output e.i. weightmap_var = 'tmin'. If none, first variable of dict keys will be used.
+    :return: output path to weightmap pickle file
+    """
+    ## Name output weightmap_file
     if weightmap_var is None:
-    ## taking first dict item for creation of weight's file.
-        weightmap_file = os.path.join(output_data_folder, f'grd2shp_weights.pickle')
+        # If non var given
+        weightmap_file = os.path.join(output_data_folder, 'grd2shp_weights.pickle')
         weightmap_var = list(xarray_dict.keys())[0]
         print(weightmap_file, weightmap_var)
-
     else:
         weightmap_file = os.path.join(output_data_folder, f'grd2shp_weights_{weightmap_var}.pickle')
         print(weightmap_file)
 
     ## Produce weightmap
     start = time.perf_counter()
-#    ds = xarray_dict[weightmap_var].load()
-
     weightmap = xa.pixel_overlaps(xarray_dict[weightmap_var], polygon)
     end = time.perf_counter()
-    print('finished agg in {} second(s)'.format(round(end - start, 2)))
+    print(f'finished agg in {round(end - start, 2)} second(s)')
 
     with open(weightmap_file, 'wb') as file:
         pickle.dump(weightmap, file)
 
     return weightmap_file
 
-create_weightmap(polygon = gdf, xarray_dict = xarray_dict, output_data_folder = output_path, weightmap_var = 'tmmx')
 
-def g2shp_regridding(xarray_dict, weightmap_file, output_data_folder, g2s_file_prefix):
+def g2shp_regridding(xarray_dict, weightmap_file, output_data_folder, g2s_file_prefix,
+                     g2s_time_var = 'day', g2s_lat_var = 'lat' , g2s_lon_var = 'lon'):
+
+    """
+    :param dict xarray_dict: dictionary of gridmet data. the output of get_gridmet_datasets()
+    :paran str weightmap_file: path to weight file for redridding
+    :param str output_data_folder: path to folder for function output
+    :param str weightmap_var: if len(xarray_dict) selected
+    :param str g2s_time_var: time variable for g2shp initialization (i.e.g2s_time_var = 'day')
+    :param str g2s_lat_var: latitude variable for g2shp initialization (i.e.g2s_time_var = 'lat')
+    :param str g2s_lon_var: time variable for g2shp initialization (i.e.g2s_time_var = 'lon')
+    :return:
+    """
 
     ## params for g2s.initialise()
     # grab long name of data vars from dict and place in list
     vars_long_list = []
     for key in xarray_dict:
         vars_long_list = vars_long_list + list(xarray_dict[key])
-    # grab short name of data vars from dict keys
+
+    # List of short names of data vars from dict keys
     vars_short_list = list(xarray_dict.keys())
+    # Lsit of xarray.datasets
     vars_grd_list = list(xarray_dict.values())
+
+    print(vars_long_list)
+    print(vars_short_list)
+    print(vars_grd_list)
 
     ## initialize Grd2ShpXagg()
     g2s = grd2shp_xagg.Grd2ShpXagg()
-
     g2s.initialize(
         grd = vars_grd_list,
         shp = gdf,
         wght_file = weightmap_file,
-        time_var = 'day',
-        lat_var = 'lat',
-        lon_var = 'lon',
+        time_var = g2s_time_var,
+        lat_var = g2s_lat_var,
+        lon_var = g2s_lon_var,
         var = vars_long_list,
         var_output = vars_short_list,
         ctype=0,
@@ -121,11 +166,39 @@ def g2shp_regridding(xarray_dict, weightmap_file, output_data_folder, g2s_file_p
     g2s.run_weights()
     end = time.perf_counter()
     print('finished agg in {} second(s)'.format(round(end - start, 2)))
+    print(g2s)
 
-    print(type(g2s))
-
+    ## output
     g2s.write_gm_file(opath=output_data_folder, prefix=g2s_file_prefix)
+
+    ## Print location of output
+    todays_date = str(datetime.datetime.now().strftime("%Y_%m_%d"))
+    file_name = g2s_file_prefix + "climate_" + todays_date + ".nc"
+    print('netcdf4 file path: ' + os.path.join(output_data_folder, file_name))
 
     return g2s
 
-g2shp_regridding(xarray_dict = xarray_dict, weightmap_file= './data/grd2shp_weights_tmmx.pickle', g2s_file_prefix='gsgrb', output_data_folder=output_path)
+####### Run function code ###
+
+# xarray_dict = get_gridmet_datasets(variable = data_vars_shrt_all,
+#                                    start_date = start_date, end_date = end_date,
+#                                    polygon_as_bbox = gdf)
+# #                     lon_min = -80, lat_min = 36, lon_max = -71, lat_max = 45
+#
+# create_weightmap(polygon = gdf,
+#                  xarray_dict = xarray_dict,
+#                  output_data_folder = output_path,
+#                  weightmap_var = 'tmmx')
+#
+#
+# g2shp_regridding(xarray_dict = xarray_dict,
+#                  weightmap_file = './data/grd2shp_weights_tmmx.pickle',
+#                  g2s_file_prefix ='tmp_tmmn_pr_',
+#                  output_data_folder = output_path)
+
+
+# subset = {key: xarray_dict[key] for key in ['tmmn', 'pr']}
+# xr_mapped = xr.open_dataset('./data/tmpclimate_2022_01_25.nc', decode_times=False)
+# gdf["tmmn"] = xr_mapped["tmmn"][:,0]
+# gdf.plot(column = 'tmmn', legend = True)
+# xr_mapped.isel(geomid=3).tmmn.plot()
